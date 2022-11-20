0dd9907a6f9150eb918b17654503087d4e3a2eef
Benjamin McLean <mclean.ben@gmail.com>
2022-11-19 20:33:49 -0600

    Initial commit

---
+++ PbpScraper/PbpHeader.cs
@@ -0,0 +1,20 @@
+﻿namespace PbpScraper
+{
+	public class PbpHeader
+	{
+		public static readonly char[] CorrectSignature = new char[4] { (char)0x00, 'P', 'B', 'P' };
+		public char[] Signature = new char[4];
+		public int Version = 0;
+		public int[] Offsets = new int[8];
+		public PbpHeader(Stream stream) : this(new BinaryReader(stream)) { }
+		public PbpHeader(BinaryReader binaryReader)
+		{
+			for (int i = 0; i < CorrectSignature.Length; i++)
+				if ((Signature[i] = binaryReader.ReadChar()) != CorrectSignature[i])
+					throw new InvalidDataException("Invalid PBP signature!");
+			Version = binaryReader.ReadInt32();
+			for (int i = 0; i < Offsets.Length; i++)
+				Offsets[i] = binaryReader.ReadInt32();
+		}
+	}
+}
